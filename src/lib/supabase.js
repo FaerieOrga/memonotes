bcc3b913db0fd47e32c2f2e987c80f76358075b8
FaerieOrga <contact@xn--farie-csa.fr>
2026-06-18 07:30:10 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // ⚠️ Remplace ces valeurs par celles de ton projet Supabase
 // (Settings > API dans le dashboard Supabase)
-const SUPABASE_URL = 'https://rbkhjpfynlghptlrzhdo.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJia2hqcGZ5bmxnaHB0bHJ6aGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDQxNjcsImV4cCI6MjA5NzEyMDE2N30.RQTD5g4m0ycV3xmFIZZB5sPv83lEjkPZTPLnkwf-Nqo'
+const SUPABASE_URL = 'https://cxsjwqjcxzmznntrmlsr.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4c2p3cWpjeHptem5udHJtbHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDM0NTYsImV4cCI6MjA5NDM3OTQ1Nn0.qjcrQ0uWww3aHUW5KobuXkRGL2Zs2_DWGO3TcoyOy9c'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
